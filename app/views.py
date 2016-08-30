a968255758dd78092566fe806281ffd979260acd
ChuckiePae <333000213@sohu.com>
2016-08-30 11:14:11 +0800

    form learning

--- app/views.py
+++ app/views.py
@@ -1,6 +1,34 @@
 from app import app
+from flask import render_template, flash, redirect
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+    user = { 'nickname': 'Chuckie' } # fake user
+    posts = [
+    	{
+    		'author': { 'nickname':'Chuckie'},
+    		'body': 'Beautiful day in Portland'
+    	},
+    	{
+    		'author':{ 'nickname':'Peter'},
+    		'body':'The Avengers movie was so cool!'
+    	}
+    ]
+    return render_template("index.html", 
+    	title='Home' ,
+    	user = user  ,
+    	posts = posts
+    	)
+
+@app.route('/login', methods = ['GET', 'POSE'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('Login requested for OpenID="' + form.Openid.data+ '", remember_me=' +str(form.remember_me.data))
+		return redirect('/index')
+	return render_template("login.html",
+		title = 'Sign in',
+		form = form
+		)
