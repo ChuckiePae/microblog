1fb65ecc99b9b735aeb4af2b465176c1a4e7ba1e
ChuckiePae <333000213@sohu.com>
2016-08-31 22:08:11 +0800

    4th about DB

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
-from app import app
-from flask import render_template, flash, redirect
 from .forms import LoginForm
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid、
+from .models import User
+
 
 @app.route('/')
 @app.route('/index')
@@ -22,13 +25,21 @@ def index():
     	posts = posts
     	)
 
-@app.route('/login', methods = ['GET', 'POSE'])
+@app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		flash('Login requested for OpenID="' + form.Openid.data+ '", remember_me=' +str(form.remember_me.data))
-		return redirect('/index')
-	return render_template("login.html",
-		title = 'Sign in',
-		form = form
-		)
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
